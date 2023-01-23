1b66d2299d987fa8095bfc160865a7fe6412c369
EC2 Default User <ec2-user@ip-172-31-9-195.ap-northeast-1.compute.internal>
2023-01-23 10:30:27 +0000

    chap-6:fin

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  before_save { self.email = self.email.downcase }
+  validates :name,  presence: true,
+                      length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                      length: {maximum: 225 },
+                      format: { with: VALID_EMAIL_REGEX },
+                  # uniqueness: { case_sensitive: false }  #大文字小文字は区別しない
+                  uniqueness: true
+
+  has_secure_password
+  validates :password, presence: true,
+                         length: { minimum: 6 }
+end
